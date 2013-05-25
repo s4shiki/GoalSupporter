5f00265a1f4c0f10f7e642ee785524af12d0c0d5
Hiromu Fujita <eda.1380@gmail.com>
2013-05-25 11:34:15 +0900

    javadoc 追加

--- src/com/example/goalsupporter/TimerActivity.java
+++ src/com/example/goalsupporter/TimerActivity.java
@@ -1,31 +1,49 @@
 package com.example.goalsupporter;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-/*
- * ここでタイマーの処理をします。
+/**
+ * タイマー画面です。タイマー関連の処理も行います。
+ * @author Hiromu Fujita
  */
 public class TimerActivity extends Activity {
-	private String TAG = "TimerActivity";//Log用string
-	private GoalData item;  //期限、一日の勉強時間、目標の値、データを保持する。GoalListからの受け取り用。
+	/**Log.v用タグ*/
+	private String TAG = "TimerActivity";
+	/**期限、一日の勉強時間、目標の値、データを保持する。GoalListからの受け取り用*/
+	private GoalData item;
+	/**一日の勉強時間のミリ秒*/
 	private long timeMsec;
-	private long interval = 1000; //タイマーのインターバル　1秒
-	private boolean offOn;//ボタンのON、OFF
-	private String time; //stop押下時の時間保持用変数
-	private String[] splitTime; //stop押下時の時間保持用変数
-	private Intent intent;//GoalListとの値のやり取り用
+	/**カウントダウンタイマーのインターバル(１秒)*/
+	private long interval = 1000;
+	/**ボタンのONとOFF用*/
+	private boolean offOn;
+	/**STOPボタン押下時の時間保持用(:を含んだ状態)*/
+	private String time;
+	/**STOPボタン押下時の時間保持用(:で配列に分ける)*/
+	private String[] splitTime;
+	/**インテント(GoalListから来てGoalActivityのActivityResultへ)*/
+	private Intent intent;
+	/**カウントダウンタイマークラス*/
 	private TimerCountDownTimer tcdt;
-
+	/**１日の勉強時間。カウントダウンタイマークラスから編集するためにpublic*/
 	public TextView timer;
+	/**全体の勉強時間。カウントダウンタイマークラスから編集するためにpublic*/
 	public TextView deadline;
+	/**ON、OFF用のボタン。カウントダウンタイマークラスから編集するためにpublic
+	 * @see TimerCountDownTimer.java
+	 */
 	public Button button;
 
+	/**
+	 * 初期化等を行う。この初期段階でResult用のIntentを用意しておく
+	 *
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,10 +58,12 @@ public class TimerActivity extends Activity {
 
 		intent = getIntent();
 		item = ((GoalData) intent.getSerializableExtra("GOALDATA"));
+		int position = intent.getIntExtra("POSITION", -1);
 
 		((TextView)findViewById(R.id.goalText)).setText( item.getGoalName() );
 
 		long goalMsec	= Integer.parseInt( item.getRemainingTime() )*60*60;
+		splitTime = item.getRemainingTime().split(":", 0);
 		deadline.setText( String.format("%d:%02d:%02d",  goalMsec/60/60, goalMsec/60%60, goalMsec%60) );
 
 		timeMsec = Integer.parseInt( item.getDayStudyTime() )*60*1000+999;
@@ -53,8 +73,13 @@ public class TimerActivity extends Activity {
 		splitTime = time.split(":", 0);
 
 		intent = new Intent();
+		intent.putExtra("POSITION", position);
 	}
 
+	/**
+	 * ボタン押下時の処理。XMLファイルの方にonCliickを指定
+	 *
+	 */
 	public void buttonClick(View view) {
 		Log.v(TAG, "buttonClick()");
 		if( (Integer.parseInt(splitTime[0]) != 0) || (Integer.parseInt(splitTime[1]) != 0) ) {
